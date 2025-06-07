60e0c9cb24fba5353e84f5046c2552988bc1fa52
moehzi <faizi1234098@gmail.com>
2025-06-07 21:36:23 +0700

    fix: fix loading and 404 after refresh

--- src/pages/detail-user/UserRepos/loading.tsx
+++ src/pages/detail-user/UserRepos/loading.tsx
@@ -5,13 +5,13 @@ const Loading = () => {
 	return (
 		<div className="flex flex-col gap-4 flex-[2]">
 			<H2>Repositories</H2>
-			<div className="flex flex-col gap-4 overflow-auto max-h-[400px] md:max-h-[600px] w-full">
+			<div className="flex flex-col gap-4 overflow-auto max-h-[400px] md:max-h-[600px]">
 				{[...Array(5)].map((_, index) => (
 					<div key={index} className="flex gap-4 h-[72px]">
 						<Skeleton className="p-3 rounded-lg w-12 h-12" />
-						<div className="flex flex-col gap-1 max-w-[800px] w-full">
+						<div className="flex flex-col gap-1 max-w-[800px]">
 							<Skeleton className="h-5 w-48" />
-							<Skeleton className="h-4 w-100" />
+							<Skeleton className="h-4 w-full" />
 						</div>
 					</div>
 				))}
